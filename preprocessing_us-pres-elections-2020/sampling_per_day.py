6c89968522508ec35ba26e9fa2d7cf85ee0909d3
CoGian <kgiantsios@atypon.com>
2021-04-28 15:54:36 +0300

    sampling

--- preprocessing_us-pres-elections-2020/sampling_per_day.py
+++ preprocessing_us-pres-elections-2020/sampling_per_day.py
@@ -1,13 +1,12 @@
-from pyspark.sql import functions as F, SparkSession
-from pyspark.sql import types as spark_types
+from pyspark.sql import SparkSession
 import os
 from tqdm import tqdm
 
-if __name__ == '__main__':
 
-    dataset_path = "us-pres-elections-2020"
+def sampling():
+    dataset_path = "../us-pres-elections-2020"
 
-    days_per_month_dir = {"09": 30, "10": 31, "11":30}
+    days_per_month_dir = {"09": 30, "10": 31, "11": 30}
 
     spark = SparkSession.builder.master("local[*]"). \
         appName("sampling"). \
@@ -18,19 +17,32 @@ if __name__ == '__main__':
             continue
         month_path = os.path.join(dataset_path, month)
         days_per_month = days_per_month_dir[month.split('-')[1]]
+        if not os.path.exists(month):
+            os.mkdir(month)
         print(month)
-        for day in tqdm(range(1, days_per_month+1)):
+        for day in tqdm(range(1, days_per_month + 1)):
             if day < 10:
-                day_str = "0" + str(1)
+                day_str = "0" + str(day)
             else:
                 day_str = str(day)
 
             day_dataset = spark.read.text(month_path + '/us-presidential-tweet-id-' + month + '-' + day_str + '-*.txt')
             day_dataset.persist()
             size = day_dataset.count()
-            sampled_day_dataset = day_dataset.sample(fraction=4000/size, seed=4)
+            sampled_day_dataset = day_dataset.sample(fraction=4000 / size, seed=4)
             day_dataset.unpersist()
-            sampled_day_dataset.coalesce(1).write.json(month + "/" + day_str, mode='overwrite')
+            twitter_ids = sampled_day_dataset.collect()
+            twitter_ids = [row['value'] for row in twitter_ids]
+            with open(month + "/" + day_str + '.txt', 'w') as fout:
+                fout.write("\n".join(twitter_ids))
+
+
+if __name__ == '__main__':
+    sampling()
+
+
+
+
 
 
 
