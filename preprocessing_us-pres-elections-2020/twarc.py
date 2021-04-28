e909dcbed90e7b0a2092218f4128416aac91e540
CoGian <kgiantsios@atypon.com>
2021-04-28 20:54:51 +0300

    made helper script more user friendly

--- preprocessing_us-pres-elections-2020/twarc.py
+++ preprocessing_us-pres-elections-2020/twarc.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+from tqdm import tqdm
 args = sys.argv[1:]
 
 if len(args)==0:
@@ -9,9 +9,11 @@ if len(args)==0:
 else:
     input_dir = args[0]
     output_dir = args[1]
-    
+
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
 files = os.listdir(input_dir)
 
-for f in files:
+for f in tqdm(files):
     filename = input_dir+'/'+f
     os.system('twarc hydrate ' + filename + ' > ' + output_dir + '/' + f + '_out.jsonl')
