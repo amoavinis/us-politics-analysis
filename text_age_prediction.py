3ca8a84d8554a58a725d5ea9fc37fba2c0cda8ab
Angelos Moavinis <amoavinis@gmail.com>
2021-05-01 01:46:15 +0300

    temp progress on ml

---
+++ text_age_prediction.py
@@ -0,0 +1,67 @@
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import f1_score, accuracy_score, mean_squared_error
+from sklearn.decomposition import TruncatedSVD
+import pickle
+import numpy as np
+import pandas as pd
+import os
+from collections import Counter
+
+def convert_age(n):
+    if n<25:
+        return 0
+    elif n<45:
+        return 1
+    elif n<65:
+        return 2
+    else:
+        return 3
+
+
+data = pd.read_csv('data/user_age_dataset.csv')
+
+X, y = data['text'], data['age']
+#y = [convert_age(n) for n in y]
+x_train, x_test, y_train, y_test = train_test_split(X, y) 
+print("Loaded and split data")
+
+vec = TfidfVectorizer()
+x_train = vec.fit_transform(x_train)
+x_test = vec.transform(x_test)
+
+svd = TruncatedSVD(n_components=512)
+x_train = svd.fit_transform(x_train)
+x_test = svd.transform(x_test)
+print("TFIDF transformation and SVD reduction completed")
+
+scaler = MinMaxScaler((-1, 1))
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+print("Scaling completed")
+
+if not os.path.exists('pretrained-models/user_age/text_age_classifier.pkl'):
+    model = MLPRegressor((100, 30), momentum=0.99, max_iter=500, random_state=0)
+    model.fit(x_train, y_train)
+    pickle.dump(model, open('pretrained-models/user_age/text_age_classifier.pkl', 'wb'))
+    print("Model trained")
+else:
+    model = pickle.load(open('pretrained-models/user_age/text_age_classifier.pkl', 'rb'))
+    print("Model loaded from pickle file")
+
+y_train_pred = model.predict(x_train)
+y_pred = model.predict(x_test)
+
+#train_f1 = f1_score(y_train, y_train_pred, average='macro')
+#train_accuracy = accuracy_score(y_train, y_train_pred)
+#test_f1 = f1_score(y_test, y_pred, average='macro')
+#test_accuracy = accuracy_score(y_test, y_pred)
+
+#print("Training F1:", round(train_f1, 2))
+#print("Training accuracy:", round(train_accuracy, 2))
+#print("Testing F1:", round(test_f1, 2))
+#print("Testing accuracy:", round(test_accuracy, 2))
+print(mean_squared_error(y_train, y_train_pred, squared=False))
+print(mean_squared_error(y_test, y_pred, squared=False))
