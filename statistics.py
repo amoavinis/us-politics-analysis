3440c2d868dd8f20d1ac3c7710c40fd27f676171
stergiosbamp <bampakis.stergios@gmail.com>
2021-05-17 21:40:42 +0300

    Add two-level statistics for gender and political

--- statistics.py
+++ statistics.py
@@ -21,5 +21,36 @@ if __name__ == "__main__":
     ax = df['subjectivity'].plot.hist()
     plt.show()
 
+    
+    # For gender find political orientation
+    print("Females:\n", df[df['gender']==1]['political'].value_counts())
+    print("Males:\n", df[df['gender']==0]['political'].value_counts())
 
-    ## df[df['gender']==1]['political'].value_counts()
+    # For gender find age
+    print("Females:\n", df[df['gender']==1]['age'].value_counts())
+    print("Males:\n", df[df['gender']==0]['age'].value_counts())
+
+    # For gender find sentiment (polarity)
+    print("Females:\n", df[df['gender']==1]['total_sentiment'].value_counts())
+    print("Males:\n", df[df['gender']==0]['total_sentiment'].value_counts())
+
+    # For gender find subjectivity
+    print("Females:\n", df[df['gender']==1]['subjectivity'].value_counts())
+    print("Males:\n", df[df['gender']==0]['subjectivity'].value_counts())
+
+    
+    # For political find gender
+    print("Republicans:\n", df[df['political']==1]['gender'].value_counts())
+    print("Democrats:\n", df[df['political']==0]['gender'].value_counts())
+
+    # For political find age
+    print("Republicans:\n", df[df['political']==1]['age'].value_counts())
+    print("Democrats:\n", df[df['political']==0]['age'].value_counts())
+
+    # For political find sentiment (polarity)
+    print("Republicans:\n", df[df['political']==1]['total_sentiment'].value_counts())
+    print("Democrats:\n", df[df['political']==0]['total_sentiment'].value_counts())
+
+    # For political find subjectivity
+    print("Republicans:\n", df[df['political']==1]['subjectivity'].value_counts())
+    print("Democrats:\n", df[df['political']==0]['subjectivity'].value_counts())
